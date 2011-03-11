48afdb3df1c81410db55542e2570080318e75d33
Ed Singleton <singletoned@gmail.com>
2011-03-11 22:16:41 +0000

    Refactored test to be generative

--- test_jade.py
+++ test_jade.py
@@ -3,26 +3,17 @@
 import jade
 
 def test_simple_tag():
-    data = "p"
-    expected = ['tag', "p"]
-    result = jade.parse(data)
-    assert expected == result
+    def do_test(data):
+        expected = ['tag', data]
+        result = jade.parse(data)
+        assert expected == result
 
-    expected = """
-<p>
-</p>
-    """.strip()
-    result = jade.to_html(data)
-    assert expected == result
+        expected = """
+<%(data)s>
+</%(data)s>
+        """.strip() % dict(data=data)
+        result = jade.to_html(data)
+        assert expected == result
 
-    data = "div"
-    expected = ['tag', "div"]
-    result = jade.parse(data)
-    assert expected == result
-
-    expected = """
-<div>
-</div>
-    """.strip()
-    result = jade.to_html(data)
-    assert expected == result
+    for tag in ["p", "div"]:
+        yield do_test, tag
