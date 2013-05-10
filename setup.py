3bc3b83b7008b28d4fe7ad3ff9c4ba51d98de659
Ed Singleton <singletoned@gmail.com>
2013-05-10 14:03:37 +0100

    Added a basic setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+data = dict(
+    name='jade',
+    version=version,
+    author="Ed Singleton",
+    author_email="singletoned@gmail.com",
+    description="A jade parser",
+    py_modules=['jade'],
+    include_package_data=True,
+    zip_safe=False,
+)
+
+if __name__ == '__main__':
+    setup(**data)
